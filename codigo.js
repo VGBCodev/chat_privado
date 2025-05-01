53462c99c1990399d13a31c6c50d8bc794972034
VGBCodev <vgbcodev@gmail.com>
2025-05-01 12:03:07 -0300

    5

--- codigo.js
+++ codigo.js
@@ -5,12 +5,13 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstati
 
 // Configuração do Firebase
 const firebaseConfig = {
-    apiKey: "Sua-API-Key",
-    authDomain: "Seu-Auth-Domain",
-    projectId: "Seu-Project-ID",
-    storageBucket: "Seu-Storage-Bucket",
-    messagingSenderId: "Seu-Messaging-Sender-ID",
-    appId: "Seu-App-ID"
+    apiKey : "AIzaSyAqAadcmcVI-hYBJjpnxdC4lxwxbZIWZks" , 
+    authDomain:"chat-privado-60dc3.firebaseapp.com", 
+    databaseURL:"https://chat-privado-60dc3-default-rtdb.firebaseio.com", 
+    projectId : "chat-privado-60dc3" , 
+    storageBucket : "chat-privado-60dc3.firebasestorage.app" , 
+    messagingSenderId : "513902320586" , 
+    appId: "1:513902320586:web:ac8d9015403360ccec0d33" 
 };
 
 // Inicializa o Firebase
